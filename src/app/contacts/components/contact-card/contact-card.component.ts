56a4ed8214fbdd39a9b6b606c68c0c308fdbb9d0
vychikovvv <vvvychikov@gmail.com>
2022-02-17 12:12:35 +0200

    added toggle and card section

--- src/app/contacts/components/contact-card/contact-card.component.ts
+++ src/app/contacts/components/contact-card/contact-card.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Contact } from '../../models/contact';
+import { Component, OnInit, Input } from "@angular/core";
+import { Contact } from "../../models/contact";
 
 @Component({
-	selector: 'app-contact-card',
-	templateUrl: './contact-card.component.html',
-	styleUrls: ['./contact-card.component.scss']
+  selector: "app-contact-card",
+  templateUrl: "./contact-card.component.html",
+  styleUrls: ["./contact-card.component.scss"],
 })
 export class ContactCardComponent implements OnInit {
+  @Input() contact: Contact;
+  public profilePhotoUrl: string;
+  constructor() {
+    this.profilePhotoUrl = "../../../../assets/images/profile-photo.png";
+  }
 
-	@Input() contact: Contact;
-	public profilePhotoUrl: string;
-	constructor() {
-		this.profilePhotoUrl = 'assets/images/photo.png';
-	}
-
-	ngOnInit() {
-	}
-
+  ngOnInit() {}
 }
